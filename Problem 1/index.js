9b94155a4d8b523223eb11672b3f10cd3d6a3c71
jake-utk <jakeryanaddis@gmail.com>
2021-02-01 23:40:30 -0600

    Problem 1 Pseudocode

--- Problem 1/index.js
+++ Problem 1/index.js
@@ -1,3 +1,13 @@
 console.log("Problem 1");
 
 // https://projecteuler.net/problem=1
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// PSEUDOCODE
+// Iterate from i = 0; i < 100; i++
+// if i is a multiple of 3 or 5, push into sum array
+// add sum array together
+// return value
